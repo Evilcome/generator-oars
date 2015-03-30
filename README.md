0e1180f38ca5f63bb6256c7acfc478969175ac7a
Evilcome <evilcome.com@gmail.com>
2015-03-30 16:46:14 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# generator-oras
-Oras generator tools for node module oras.
+# generator-oars
+Oars generator tools for node module oars.
+
+> [Yeoman](http://yeoman.io) generator that scaffolds out a front-end web app using [gulp](http://gulpjs.com/) for the build process
+
+![](screenshot.png)
+
+## Getting Started
+
+- Install dependencies: `npm install --global yo bower`
+- Install the generator: `npm install --global generator-oars`
+- Run `yo oars` to scaffold your webapp
+- Run `npm start` to launch your webapp
+
+## Options
+
+- `--skip-welcome-message`
+  Skips Yeoman's greeting before displaying options.
+- `--skip-install-message`
+  Skips the the message displayed after scaffolding has finished and before the dependencies are being installed.
+- `--skip-install`
+  Skips the automatic execution of `bower` and `npm` after scaffolding has finished.
+- `--test-framework=<framework>`
+  Defaults to `mocha`. Can be switched for another supported testing framework like `jasmine`.
---
+++ app/index.js
@@ -0,0 +1,104 @@
+'use strict'
+var fs = require('fs');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+module.exports = yeoman.generators.Base.extend({
+
+	constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.option('test-framework', {
+      desc: 'Test framework to be invoked',
+      type: String,
+      defaults: 'mocha'
+    });
+
+    this.option('skip-welcome-message', {
+      desc: 'Skips the welcome message',
+      type: Boolean
+    });
+
+    this.option('skip-install', {
+      desc: 'Skips the installation of dependencies',
+      type: Boolean
+    });
+
+    this.option('skip-install-message', {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
+    });
+  },
+
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay('\'Allo \'allo! Out of the box I include Oars, Restify, and a gulpfile.js to build your app.'));
+    }
+  },
+
+  writing: {
+    index: function() {
+      this.template('index.js')
+    },
+
+  	gulpfile: function () {
+      this.template('gulpfile.js');
+    },
+
+    packageJSON: function () {
+      this.template('_package.json', 'package.json');
+    },
+
+    git: function () {
+      this.copy('gitignore', '.gitignore');
+    },
+
+    app: function () {
+      this.mkdir('api');
+      this.mkdir('api/controllers');
+      this.mkdir('api/models');
+      this.mkdir('api/policies');
+      this.mkdir('api/responses');
+      this.mkdir('api/services');
+
+      this.mkdir('config');
+      this.mkdir('tasks');
+    }
+  },
+
+  install: function () {
+    var howToInstall =
+      '\nAfter running ' +
+      chalk.yellow.bold('npm install') +
+      '.';
+
+    if (this.options['skip-install']) {
+      this.log(howToInstall);
+      return;
+    }
+
+    this.installDependencies({
+      skipMessage: this.options['skip-install-message'],
+      skipInstall: this.options['skip-install']
+    });
+
+    this.on('end', function () {
+
+      // ideally we should use composeWith, but we're invoking it here
+      // because generator-mocha is changing the working directory
+      // https://github.com/yeoman/generator-mocha/issues/28
+      this.invoke(this.options['test-framework'], {
+        options: {
+          'skip-message': this.options['skip-install-message'],
+          'skip-install': this.options['skip-install']
+        }
+      });
+    }.bind(this));
+  }
+});
