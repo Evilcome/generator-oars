73607b05eea1802574e9f95f68f69988dcc06349
Evilcome <evilcome.com@gmail.com>
2015-05-14 13:39:15 +0800

    fix session bug

--- app/templates/policies/sessionAuth.js
+++ app/templates/policies/sessionAuth.js
@@ -6,23 +6,13 @@
  *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
  *
  */
-var session = require('../../libs/session');
 
 module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller.
-  if (req.session.authenticated) {
+  if (req.session && req.session.authenticated) {
     return next();
-  }
-
-  // clean the sid, fix too much sid problem.
-  if (req.session.sid) {
-  	session.destroy(req.session.sid, function(err) {
-  		next.ifError(err);
-
-  		next();
-  	})
   }
 
   // User is not allowed
